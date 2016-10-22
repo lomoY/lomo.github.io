9194b57bfa182568c3cfb775c3166cf53d78819a
Lomo <Lomozhenyu@163.com>
2016-10-23 00:07:21 +0800

    main page carousel add

---
+++ _site/js/lomoslick.js
@@ -0,0 +1,11 @@
+    $(document).ready(function() {
+        $('.your-class').slick({
+            dots: true,
+      
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: true,
+            fade: true,
+            cssEase: 'linear'
+        });
+    });
